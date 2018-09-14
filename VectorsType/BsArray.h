dd0f32064e593455dc0d449c2041dd1a6958871f
paolopaoletto <35465600+paolopaoletto@users.noreply.github.com>
2018-09-14 13:10:27 +0200

    Update BsArray.h

--- VectorsType/BsArray.h
+++ VectorsType/BsArray.h
@@ -253,7 +253,7 @@ namespace bs
 	}
 
 	template <class Type>
-	void Array<Type>::smallAlloc(UINT32 numElements, bool keepData)
+	void Array<Type>::smallAlloc(UINT32 numElements, bool data)
 	{
 		Type* tmp = 0;
 
